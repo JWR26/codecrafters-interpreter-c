7b8a21450b5925cf56a8ddeacf1041603a024d20
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2024-11-04 13:55:17 +0100

    lexeme length for string updated

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -302,7 +302,7 @@ int scan_tokens(TokenArray *a, char *source)
                 t->type = STRING;
                 t->lexeme = source;
                 int l = (temp - source);
-                t->length = l;
+                t->length = l+1;
                 t->string = (char *)malloc(l * sizeof(char));
                 strncpy(t->string, source+1, l-1);
                 t->string[l-1] = '\n';
