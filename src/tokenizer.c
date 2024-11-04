183ad7c8a6757b3db2fe531d937ab514c56b596b
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2024-11-04 13:54:28 +0100

    string identified

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -117,7 +117,7 @@ void print_token(const Token *t)
     printf("%s ", token_type_as_string(t->type));
     for(int i = 0; i < t->length; ++i)
     {
-        printf("%c ", t->lexeme[i]);
+        printf("%c", t->lexeme[i]);
     }
     printf(" ");
     if (t->string != NULL)
